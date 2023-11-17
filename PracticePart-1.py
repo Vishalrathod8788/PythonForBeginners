f802c40d9d0653e5d5f9f221d6f307771f19c8ae
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-17 13:09:28 +0530

    Multipy on pro and Qty

--- PracticePart-1.py
+++ PracticePart-1.py
@@ -1,6 +1,18 @@
-price_product_1 = input("What is the Price of the Product")
-quantity_product_1 = input("What is the Quantityof the Product")
-price_product_3 = input("What is the Price of the Product")
-quantity_product_3 = input("What is the Quantityof the Product")
-price_product_3 = input("What is the Price of the Product")
-quantity_product_3 = input("What is the Quantityof the Product")
+price_product_1 = input("What is the Price of the Product ")
+quantity_product_1 = input("What is the Quantityof the Product ")
+price_product_2 = input("What is the Price of the Product ")
+quantity_product_2 = input("What is the Quantityof the Product ")
+price_product_3 = input("What is the Price of the Product ")
+quantity_product_3 = input("What is the Quantityof the Product ")
+
+result_product_1 = float(price_product_1) * int(quantity_product_1)
+
+print(result_product_1)
+
+result_product_2 = float(price_product_2) * int(quantity_product_2)
+
+print(result_product_2)
+
+result_product_3 = float(price_product_3) * int(quantity_product_3)
+
+print(result_product_3)
