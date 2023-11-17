52f737c8226bda8ee73cec4ac4efa28373f3d101
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-17 12:48:39 +0530

    ...

--- Input_fun.py
+++ Input_fun.py
@@ -1,6 +1,1 @@
 name = input("What is Your Name ")
-age = input("What is Your Age ")
-food = input("What is Your Favorite Food ")
-drink = input("What is Your Favorite Drink ")
-
-print("Hello " + name + "! You are " + age + " years old. Your favorite food is " + food + " and your favorite drink is " + drink + ".")
