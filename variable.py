5491fc92d3312a8a0b473ee9fba6ea192a01cfb0
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-17 12:08:07 +0530

    All Variable

--- variable.py
+++ variable.py
@@ -1,3 +1,26 @@
+# string
 name = "VGRcoder"
 print(name)
 
+# int
+number = 88
+print(number)
+
+# boolean
+Having_a_house = True
+if Having_a_house:
+    print("I own a Small House")
+else:
+    print("I don't own a house")
+
+# charactor
+char = 'A'
+print(char)
+
+# float
+num = 100.01
+print(num)
+
+# Double
+waight = 55.588558
+print(waight)
