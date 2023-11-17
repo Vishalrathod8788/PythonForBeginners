afdcd6a5ca0e66d50642ce3a7cd9b403a27c8414
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-17 12:17:15 +0530

    StringManipulate

---
+++ StringManipulation.py
@@ -0,0 +1,17 @@
+sentence = "Hello Guys"
+
+print(sentence)
+
+print(sentence.upper())
+
+print(sentence.lower())
+
+print(sentence.title())
+
+print(sentence.lower().islower())
+
+print(len(sentence))
+
+print(sentence.replace("Hello Guys" , "Hey !"))
+
+print(sentence.index("y"))
