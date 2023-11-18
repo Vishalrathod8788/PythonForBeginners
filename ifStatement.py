ddfd7e3bd9e8d71956af819522a753465605b7ef
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 16:49:34 +0530

    Ad...

--- ifStatement.py
+++ ifStatement.py
@@ -7,3 +7,5 @@ wallet_2 = input("How much money do you have ")
 name_3 = input("What is Your Name : ")
 wallet_3 = input("How much money do you have ")
 
+if wallet_1 > wallet_2 and wallet_1 > wallet_3:
+    print(name_1, "is the richest person")
