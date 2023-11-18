ebd3f8a87c1e03c535f2a53652714f1dac29deb3
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 17:10:04 +0530

    Add

--- QuestionAnswer.py
+++ QuestionAnswer.py
@@ -2,3 +2,5 @@ def question():
     rules = input("You need to answer every question by yes or no do you understand : ")
     if rules != "yes":
         return print("try again")
+    else:
+        print("Next Question : ")
