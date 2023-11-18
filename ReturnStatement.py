132f69b25277d37efd6d88cf0a4ea5f36a14299a
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 12:52:32 +0530

    ReturnStatement

--- ReturnStatement.py
+++ ReturnStatement.py
@@ -1,6 +1,11 @@
 def calculate(a, b, c):
+    print("Addition")
     print(a + b + c)
+    print("Multiplication ")
     print(a * b * c)
+    
+    return
+    print("Something...")
     print(a - b - c)
     print(a * b / c)
 
