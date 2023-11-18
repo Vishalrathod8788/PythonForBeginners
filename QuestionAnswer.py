6fc3171e2cba50f43d1cfd570cc143df8d1cd48d
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 17:10:12 +0530

    Add

--- QuestionAnswer.py
+++ QuestionAnswer.py
@@ -4,3 +4,6 @@ def question():
         return print("try again")
     else:
         print("Next Question : ")
+    question_1 = input("Are you hungry : ")
+    if question_1 != "yes":
+        return print("Then let's go for walk")
