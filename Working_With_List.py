3e65f980bc838f9aa58871d971045a032ce8eb0a
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 17:39:44 +0530

    Add... index

--- Working_With_List.py
+++ Working_With_List.py
@@ -1,7 +1,15 @@
 variable_1 = "Apple"
 
 list_1 = ["Apple", "Banana", "Orange"]
+list_1[0]="Mango"
 
-print(list_1)
+print(list_1[0:2])
+
+# list_1.append("Mango")
+# list_1.remove("Apple")
+# list_1.pop()
+# list_1.pop(0)
+# list_1.clear()
+
 
 del list_1
