da89eae4b2fb6bbbf218c2fc0502ee0cda7d4091
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-18 16:50:47 +0530

    Add...

--- ifStatement.py
+++ ifStatement.py
@@ -9,3 +9,7 @@ wallet_3 = input("How much money do you have ")
 
 if wallet_1 > wallet_2 and wallet_1 > wallet_3:
     print(name_1, "is the richest person")
+elif wallet_2 > wallet_1 and wallet_2 > wallet_3:
+    print(name_2, "is the richest person")
+else:
+    print(name_3, "is the richest person")
