907ddc234157e02b25c505aa2737cb8ae79371a2
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-19 16:46:39 +0530

    Difference List & Tuples

--- Diff_WIth_List_Tuples.py
+++ Diff_WIth_List_Tuples.py
@@ -1,9 +1,13 @@
 
 # list in any Items are Changeable
 list = ["A", "B", "C", "D"]
+list_num = [1, 2, 3, 4]
 
 # Tuples in All Items Are Static That mean Fixed
-color = ("One", "Two", "Three", "Four", "Five")
+color = ("Red", "Green", "Gray", "Pink", "Black")
+color_num = (1, 2, 3, 4, 5, 6, 7)
 
 list.insert(4,"E")
 print(list)
+
+
