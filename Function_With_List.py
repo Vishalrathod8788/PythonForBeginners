90b8b3f17a63f397b3c1f73eec18ca17728eea6b
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-19 13:34:18 +0530

    Function with list

--- Function_With_List.py
+++ Function_With_List.py
@@ -1,8 +1,18 @@
-food = ["humburger", "Pizza", "Juice", "Fries", "Sushi"]
+food = ["humburger", "Pizza", "Juice", "Fries", "Sushi", "Sushi", "Sushi"]
 
 price = [55, 25, 20, 30, 15]
 
 food.insert(2, "Sandwich")
 
-food.extend(price)
+# food.extend(price)
+
+
+print(food.index("Sushi"))
+
+food.remove("Pizza")
 print(food)
+print(food.count("Sushi"))
+
+random = food.copy() + price.copy()
+
+print(random)
