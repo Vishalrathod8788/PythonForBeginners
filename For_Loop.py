7548995aa27c3d5fa9c97d97f866432147c5a95f
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-21 05:18:14 +0530

    Traffice Lite

---
+++ For_Loop.py
@@ -0,0 +1,20 @@
+colors = ["Blue", "Red", "Green", "Gray", "Pink"]
+
+for one_color in colors:
+    print(one_color)
+
+for letter in "Hello":
+    print(letter)
+
+
+# Traffic Lite Color 
+colors = ["Red", "Yellow", "Green"]
+
+for color in colors:
+    print(color)
+    if color == "Green":
+        print("Go")
+    elif color == "Yellow":
+        print("Wait")
+    else:
+        print("Stop")
