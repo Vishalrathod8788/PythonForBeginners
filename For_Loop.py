dd53e81b931676abecb449998caf57450d43dff8
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-21 05:21:38 +0530

    ...

--- For_Loop.py
+++ For_Loop.py
@@ -1,16 +1,18 @@
-for letter in "Hello":
-    print(letter)
+# for letter in "Hello":
+#     print(letter)
 
+# # Traffic Lite Color 
+# colors = ["Red", "Yellow", "Green"]
 
-# Traffic Lite Color 
-colors = ["Red", "Yellow", "Green"]
+# for color in colors:
+#     print(color)
+#     if color == "Green":
+#         print("Go")
+#         break
+#     elif color == "Yellow":
+#         print("Wait")
+#     else:
+#         print("Stop")
 
-for color in colors:
-    print(color)
-    if color == "Green":
-        print("Go")
-        break
-    elif color == "Yellow":
-        print("Wait")
-    else:
-        print("Stop")
+for i in range(1, 11):
+    print(i)
