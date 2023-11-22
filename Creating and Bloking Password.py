5775654adbd05141b65dfde55b434c830d947fa9
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-23 04:37:55 +0530

    Add.....

--- Creating and Bloking Password.py
+++ Creating and Bloking Password.py
@@ -5,4 +5,13 @@ num_of_try_max = 3
 max_try = "not Reached"
 
 while your_password != our_password and max_try != "Reached ":
-    
+    if num_of_try < num_of_try_max:
+        your_password = input("Enter your password: ")
+        num_of_try += 1
+        if your_password != our_password:
+            print("Wrong password")
+            print("You have " + str(num_of_try_max - num_of_try) + " try left")
+            max_try = "Reached"
+
+        else:
+            print("Welcome")            
