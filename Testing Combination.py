408523adb8c973d5ccf1d0986c0810db52ba3b40
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-23 07:07:41 +0530

    Add... Traigle

--- Testing Combination.py
+++ Testing Combination.py
@@ -1,4 +1,17 @@
-for i in range(1, 6):
+n = input("Enter Digit...")
+
+
+
+
+for i in range(int(n)):
+    for j in range(int(n)-i):
+        print(" ", end="    ")
+    for k in range(i):
+        print("*", end="    ")
+    print()
+    print()
+
+for i in range(int(n)):
     for j in range(i):
         print("*", end="    ")
     print()
