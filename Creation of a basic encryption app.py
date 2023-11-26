d7a0e5f8bf3187877e292f17a358a04c19bed6e6
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-26 11:37:39 +0530

    Add...

--- Creation of a basic encryption app.py
+++ Creation of a basic encryption app.py
@@ -1,3 +1,25 @@
 def created(sentence):
     trasnslation = ""
     for element in sentence:
+        if element in "Aa":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
+        elif element in "Bb":
+            trasnslation = trasnslation + "1"
