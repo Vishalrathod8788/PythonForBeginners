713f80a61e763408dd117103cf4cc86105a8ce53
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-27 11:48:08 +0530

    Encripted Password...

--- Creation of a basic encryption app.py
+++ Creation of a basic encryption app.py
@@ -9,50 +9,50 @@ def created(sentence):
             trasnslation = trasnslation + "3"
         elif element in "Dd":
             trasnslation = trasnslation + "4"
-        elif element in "Bb":
+        elif element in "Ee":
             trasnslation = trasnslation + "5"
-        elif element in "Bb":
+        elif element in "Ff":
             trasnslation = trasnslation + "6"
-        elif element in "Bb":
+        elif element in "Gg":
             trasnslation = trasnslation + "7"
-        elif element in "Bb":
+        elif element in "Hh":
             trasnslation = trasnslation + "8"
-        elif element in "Bb":
+        elif element in "Ii":
             trasnslation = trasnslation + "9"
-        elif element in "Bb":
+        elif element in "Jj":
             trasnslation = trasnslation + "!"
-        elif element in "Bb":
+        elif element in "Kk":
             trasnslation = trasnslation + "@"
-        elif element in "Bb":
+        elif element in "Ll":
             trasnslation = trasnslation + "#"
-        elif element in "Bb":
+        elif element in "Mm":
             trasnslation = trasnslation + "$"
-        elif element in "Bb":
+        elif element in "Nn":
             trasnslation = trasnslation + "%"
-        elif element in "Bb":
+        elif element in "Oo":
             trasnslation = trasnslation + "^"
-        elif element in "Bb":
+        elif element in "Pp":
             trasnslation = trasnslation + "&"
-        elif element in "Bb":
+        elif element in "Qq":
             trasnslation = trasnslation + "*"
-        elif element in "Bb":
+        elif element in "Rr":
             trasnslation = trasnslation + "("
-        elif element in "Bb":
+        elif element in "Ss":
             trasnslation = trasnslation + ")"
-        elif element in "Bb":
+        elif element in "Tt":
             trasnslation = trasnslation + "-"
-        elif element in "Bb":
+        elif element in "Vv":
             trasnslation = trasnslation + "+"
-        elif element in "Bb":
+        elif element in "Ww":
             trasnslation = trasnslation + "|"
-        elif element in "Bb":
+        elif element in "Xx":
             trasnslation = trasnslation + "]"
-        elif element in "Bb":
+        elif element in "Yy":
             trasnslation = trasnslation + "["
-        elif element in "Bb":
+        elif element in "Zz":
             trasnslation = trasnslation + "{"
-        elif element in "Bb":
-            trasnslation = trasnslation + "}"
         else:
             trasnslation = trasnslation + element
     return trasnslation
+
+print(created("vishalrathod8788"))
