014fe6c511a9b66e320f5227e033ccbe3719bbf4
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-11-27 11:43:55 +0530

    Add

--- Creation of a basic encryption app.py
+++ Creation of a basic encryption app.py
@@ -4,42 +4,52 @@ def created(sentence):
         if element in "Aa":
             trasnslation = trasnslation + "1"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "2"
+        elif element in "Cc":
+            trasnslation = trasnslation + "3"
+        elif element in "Dd":
+            trasnslation = trasnslation + "4"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "5"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "6"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "7"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "8"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "9"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "!"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "@"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "#"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "$"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "%"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "^"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "&"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "*"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "("
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + ")"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "-"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "+"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "|"
         elif element in "Bb":
-            trasnslation = trasnslation + "1"
+            trasnslation = trasnslation + "]"
+        elif element in "Bb":
+            trasnslation = trasnslation + "["
+        elif element in "Bb":
+            trasnslation = trasnslation + "{"
+        elif element in "Bb":
+            trasnslation = trasnslation + "}"
