22296ab6cbacf2fe61568d3c7c048d42a7ce6fe7
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-01-18 20:04:23 +0530

    Add Data Member

--- ClsAndObj.py
+++ ClsAndObj.py
@@ -4,3 +4,10 @@ class BaseClass :
         self.NAME = Name
 
 class DirClass (BaseClass):
+    def __init__(self, Id, Name, Dir):
+        super().__init__(Id, Name)
+        self.DIR = Dir
+        print(self.ID)
+        print(self.NAME)
+        print(self.DIR)
+        
